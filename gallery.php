9247f35e68b5367cffc73e9ea1cacc7f7c30520e
chenoajai <winsteadchenoa@gmail.com>
2016-11-14 15:30:54 -0700

    added images and headings

---
+++ gallery.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <?php require_once ("lib/headUtils.php");?>
+    <link rel="stylesheet" href="lib/css/style.css">
+  </head>
+  <body>
+    <div class="jumbotron">
+      <div class="container">
+      <h1>New Mexico</h1>
+      <h1>Wildlife Federation</h1>
+      <h1>Gen Out Youth Club</h1>
+      </div>
+    </div>
+
+
+    <div class="row push">
+      <div class="col-md-4 middle col-xs-2">
+        <a href="#"><button type="button" class="circle1" name="button">Sign Up</button></a>
+      </div>
+
+      <div class="col-md-4 middle col-xs-8">
+        <img src="lib/images/logo.png" class="logo img-fluid animated bounceInUp" alt="" />
+      </div>
+
+      <div class="col-md-4 middle col-xs-2">
+        <a href="#"><button type="button" class="circle1" name="button">Learn More</button></a>
+      </div>
+    </div>
+
+    <div class="container picture">
+        <div class="row">
+          <div class="col-md-4">
+            <h3>RIVER TRIP</h3>
+            <img src="lib/images/rivertrip1.png" class="img-responsive center-block" alt="">
+          </div>
+
+          <div class="col-md-4">
+            <h3>WILDLIFE</h3>
+            <img src="lib/images/wildlifepic1.jpg" class="img-responsive center-block" alt="">
+          </div>
+
+          <div class="col-md-4">
+            <h3>RIVER TRIP</h3>
+            <img src="lib/images/rivertrip1.png" class="img-responsive center-block" alt="">
+          </div>
+        </div>
+
+          <hr>
+          <div class="row">
+            <div class="col-md-4">
+             <h3>WILD LIFE</h3>
+             <img src="lib/images/wildlifepic1.jpg" class="img-responsive center-block" alt="">
+            </div>
+
+            <div class="col-md-4">
+              <h3>RIVER TRIP</h3>
+              <img src="lib/images/rivertrip1.png" class="img-responsive center-block" alt="">
+            </div>
+
+            <div class="col-md-4">
+             <h3>WILD LIFE</h3>
+             <img src="lib/images/wildlifepic1.jpg" class="img-responsive center-block" alt="">
+           </div>
+          </div>
+
+       </div>
+
+
+    <?php require_once ("lib/partials/scripts.php");?>
+
+  </body>
+</html>
