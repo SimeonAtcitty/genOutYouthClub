b2af1f4178687b78269ced4c3d1b91b820cf3c40
hawkeyewoman <hawkeyewoman2013@gmail.com>
2016-11-14 14:32:52 -0700

    Template to project

--- index.php
+++ index.php
@@ -4,8 +4,31 @@
     <meta charset="utf-8">
     <title></title>
     <?php require_once ("lib/headUtils.php");?>
+    <link rel="stylesheet" href="lib/css/style.css">
   </head>
   <body>
+    <div class="jumbotron">
+      <div class="container">
+      <h1>New Mexico</h1>
+      <h1>Wildlife Federation</h1>
+      <h1>Gen Out Youth Club</h1>
+      </div>
+    </div>
+
+
+    <div class="row push">
+      <div class="col-md-4 middle col-xs-2">
+        <a href="#"><button type="button" class="circle1" name="button">Sign Up</button></a>
+      </div>
+
+      <div class="col-md-4 middle col-xs-8">
+        <img src="lib/images/logo.png" class="logo img-fluid animated bounceInUp" alt="" />
+      </div>
+
+      <div class="col-md-4 middle col-xs-2">
+        <a href="#"><button type="button" class="circle1" name="button">Learn More</button></a>
+      </div>
+    </div>
 
 
     <?php require_once ("lib/partials/scripts.php");?>
