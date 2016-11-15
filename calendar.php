63d336591b07a177b9ab0b5982abec38bb87c11f
rasmith78 <roselynsmith78@gmail.com>
2016-11-15 11:12:47 -0700

    made changes to calendar

---
+++ calendar.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <?php require_once ("lib/headUtils.php");?>
+    <link rel="stylesheet" href="lib/css/style.css">
+    <link rel="stylesheet" href="lib/css/calendar.css">
+
+  </head>
+  <body>
+    <div class="jumbotron">
+      <div class="container">
+      <h1>New Mexico</h1>
+      <h1>Wildlife Federation</h1>
+      <h1>Gen Out Youth Club</h1>
+      </div>
+    </div>
+
+
+    <div class="row push">
+      <div class="col-md-4 middle col-xs-2">
+        <a href="#"><button type="button" class="circle1" name="button">Sign Up</button></a>
+      </div>
+
+      <div class="col-md-4 middle col-xs-8">
+        <img src="lib/images/logo.png" class="logo img-fluid animated bounceInUp" alt="" />
+      </div>
+
+      <div class="col-md-4 middle col-xs-2">
+        <a href="#"><button type="button" class="circle1" name="button">Learn More</button></a>
+      </div>
+    </div>
+
+
+
+
+    <div class="container event">
+      <h1>UPCOMING EVENTS</h1>
+      <h2>Sign up and save the date</h2>
+    </div>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4">
+          <div class="card card-block" style="width: 20rem;">
+            <h3 class="card-title">December 18, 2016</h3>
+            <h3>Marathon</h3>
+              <p class="card-text">Event details and schedule
+                The Grand Valley Marathon, 1/2 Marathon, 10K and 5K is a fund raiser for the Palisade Fruit and Wine Byway.  This race was designed to encourage the whole family to come and participate in a great day of activity, regardless of their interest or running ability.  The Grand Valley Marathon is held in conjunction with the Palisade Brews and Cruise Festival.  May is Blossom season for Palisade and a great time to go wine tasting, floating and biking in Palisade and the Grand Valley.</p>
+              <a href="#" class="btn btn-primary">Sign Up Here</a>
+          </div>
+        </div>
+
+
+
+
+      <div class="row">
+        <div class="col-sm-4">
+         <div class="card card-block" style="width: 20rem;">
+          <h3 class="card-title">January 12, 2017</h3>
+            <h3>Fundraising Event</h3>
+            <p class="card-text">You will appreciate this special event, this event will help fundraise the program assisting youths. This banquet is the premier fundraising event of the fall, benefitting character clubs on public school campuses in the Farmington region.
+              Guests enjoy a live jazz band,and silent auction at the Pre-Party, followed by a grreat dinner, student success stories and a scholarship award presentation.</p>
+            <a href="#" class="btn btn-primary">Sign Up Here</a>
+          </div>
+        </div>
+
+
+
+
+        <div class="row">
+          <div class="col-md-4">
+            <div class="card card-block" style="width: 20rem;">
+              <h3 class="card-title">February 22, 2017</h3>
+                <h3>Tournament</h3>
+                <p class="card-text">Chess tournament, bring your skills, and win trophies. Meet new people that have the same future goals.4-SS, G/25 d5. Open to 2000+ players. $725 GTD: 250-150-100. Top U2400 100, Top U2300 75, Biggest upset $50. EF: $40, Mbr $30. GMs Free. Reg.: 6:15-6:45pm. Rds.: 7-8:15-9:30-10:45pm. Max one bye, for round 1 or 4 only. Request at entry. www.marshallchessclub.org. 23 W. 10th St., NYC. 212-477-3716.</p>
+                <a href="#" class="btn btn-primary">Sign Up Here</a>
+              </div>
+            </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+
+    <?php require_once ("lib/partials/scripts.php");?>
+
+  </body>
+</html>
