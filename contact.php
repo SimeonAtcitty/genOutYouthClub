f1504a9babb5edee9702ae41fcda04c3ede3a7e0
SimeonAtcitty <atcittysimo@gmail.com>
2016-11-15 11:11:41 -0700

    added stuff to contact

---
+++ contact.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <?php require_once ("lib/headUtils.php");?>
+
+    <link rel="stylesheet" href="lib/css/contact2.css">
+    <link href="https://fonts.googleapis.com/css?family=PT+Mono" rel="stylesheet">
+  </head>
+  <body>
+    <div class="jumbotron">
+      <div class="container">
+      <h1>New Mexico</h1>
+      <h1>Wildlife Federation</h1>
+      <h1>Gen Out Youth Club</h1>
+      </div>
+    </div>
+
+
+    <div class="row push">
+      <div class="col-md-4 middle col-xs-2">
+        <a href="#"><button type="button" class="circle1" name="button">Sign Up</button></a>
+      </div>
+
+      <div class="col-md-4 middle col-xs-8">
+        <img src="lib/images/logo.png" class="logo img-fluid animated bounceInUp" alt="" />
+      </div>
+
+      <div class="col-md-4 middle col-xs-2">
+        <a href="#"><button type="button" class="circle1" name="button">Learn More</button></a>
+      </div>
+    </div>
+
+    <div class="container top">
+      <h1>Contact</h1>
+    </div>
+
+    <section>
+      <div class="container info">
+        <div class="row">
+          <div class="col-md-4">
+            <div class="card">
+              <h3 class="card-header">Address</h3>
+              <div class="card-block">
+                <h4 class="card-title">100 Main St. Albuquerque, NM 87111</h4>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+              <div>
+
+              </div>
+          </div>
+          <div class="col-md-4">
+            <div class="card">
+              <h3 class="card-header">Phone</h3>
+              <div class="card-block">
+                <h4 class="card-title">505-555-5555</h4>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="card">
+              <h3 class="card-header">Email</h3>
+              <div class="card-block">
+                <h4 class="card-title">info@genoutyouthclub.com</h4>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+
+    <?php require_once ("lib/partials/scripts.php");?>
+
+  </body>
+</html>
