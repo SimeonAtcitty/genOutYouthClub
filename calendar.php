c91f6100937a0253a2494cc6fd987ddf016de124
rasmith78 <roselynsmith78@gmail.com>
2016-11-16 10:21:38 -0700

    made changes to php css

--- calendar.php
+++ calendar.php
@@ -4,11 +4,14 @@
     <meta charset="utf-8">
     <title></title>
     <?php require_once ("lib/headUtils.php");?>
-    <link rel="stylesheet" href="lib/css/style.css">
+    <link rel="stylesheet" href="lib/css/navbar.css">
+    <link rel="stylesheet" href="lib/css/index.css">
     <link rel="stylesheet" href="lib/css/calendar.css">
 
   </head>
   <body>
+    <?php require_once ("lib/partials/navbar.php");?>
+
     <div class="jumbotron">
       <div class="container">
       <h1>New Mexico</h1>
@@ -17,27 +20,26 @@
       </div>
     </div>
 
-
-    <div class="row push">
-      <div class="col-md-4 middle col-xs-2">
-        <a href="#"><button type="button" class="circle1" name="button">Sign Up</button></a>
+    <div class="col-md-4 side">
+        <figure class="circle">
+          <img src="lib/images/rivertrip1.png" alt="sq-sample14" />
+          <figcaption><a href="#"><p>Sign up</p></a></figcaption>
+          <a href="#"></a>
+        </figure>
+    </div>
+      <div class="col-md-4 push">
+           <img src="lib/images/logo.png" class="logo img-fluid animated bounceInUp" alt="" />
       </div>
-
-      <div class="col-md-4 middle col-xs-8">
-        <img src="lib/images/logo.png" class="logo img-fluid animated bounceInUp" alt="" />
-      </div>
-
-      <div class="col-md-4 middle col-xs-2">
-        <a href="#"><button type="button" class="circle1" name="button">Learn More</button></a>
-      </div>
+    <div class="col-md-4 side">
+        <figure class="circle">
+          <img src="lib/images/rivertrip1.png" alt="sq-sample14" />
+          <figcaption><a href="#"><p>Learn More</p></a></figcaption>
+          <a href="#"></a>
+        </figure>
     </div>
 
-
-
-
-    <div class="container event">
-      <h1>UPCOMING EVENTS</h1>
-      <h2>Sign up and save the date</h2>
+    <div class="col-md-12">
+      <h2>Events</h2>
     </div>
 
     <div class="container">
@@ -45,37 +47,28 @@
         <div class="col-md-4">
           <div class="card card-block" style="width: 20rem;">
             <h3 class="card-title">December 18, 2016</h3>
-            <h3>Marathon</h3>
+            <h3  class="card-title">Marathon</h3>
               <p class="card-text">Event details and schedule
-                The Grand Valley Marathon, 1/2 Marathon, 10K and 5K is a fund raiser for the Palisade Fruit and Wine Byway.  This race was designed to encourage the whole family to come and participate in a great day of activity, regardless of their interest or running ability.  The Grand Valley Marathon is held in conjunction with the Palisade Brews and Cruise Festival.  May is Blossom season for Palisade and a great time to go wine tasting, floating and biking in Palisade and the Grand Valley.</p>
-              <a href="#" class="btn btn-primary">Sign Up Here</a>
+                The Grand Valley Marathon, 1/2 Marathon, 10K and 5K is a fund raiser for the Palisade Fruit and Wine Byway.  This race was designed to encourage the whole family to come and participate in a great day of activity, regardless of their interest or running ability.  The Grand Valley Marathon is held in conjunction with the Palisade Brews and Cruise Festival. </p>
           </div>
         </div>
-
-
-
 
       <div class="row">
-        <div class="col-sm-4">
+        <div class="col-md-4">
          <div class="card card-block" style="width: 20rem;">
           <h3 class="card-title">January 12, 2017</h3>
-            <h3>Fundraising Event</h3>
+            <h3 class="card-title">Fundraising Event</h3>
             <p class="card-text">You will appreciate this special event, this event will help fundraise the program assisting youths. This banquet is the premier fundraising event of the fall, benefitting character clubs on public school campuses in the Farmington region.
               Guests enjoy a live jazz band,and silent auction at the Pre-Party, followed by a grreat dinner, student success stories and a scholarship award presentation.</p>
-            <a href="#" class="btn btn-primary">Sign Up Here</a>
           </div>
         </div>
-
-
-
 
         <div class="row">
           <div class="col-md-4">
             <div class="card card-block" style="width: 20rem;">
               <h3 class="card-title">February 22, 2017</h3>
-                <h3>Tournament</h3>
-                <p class="card-text">Chess tournament, bring your skills, and win trophies. Meet new people that have the same future goals.4-SS, G/25 d5. Open to 2000+ players. $725 GTD: 250-150-100. Top U2400 100, Top U2300 75, Biggest upset $50. EF: $40, Mbr $30. GMs Free. Reg.: 6:15-6:45pm. Rds.: 7-8:15-9:30-10:45pm. Max one bye, for round 1 or 4 only. Request at entry. www.marshallchessclub.org. 23 W. 10th St., NYC. 212-477-3716.</p>
-                <a href="#" class="btn btn-primary">Sign Up Here</a>
+                <h3 class="card-title">Tournament</h3>
+                <p class="card-text">Chess tournament, bring your skills, and win trophies. Meet new people that have the same future goals.4-SS, G/25 d5. Open to 2000+ players. $725 GTD: 250-150-100. Top U2400 100, Top U2300 75, Biggest upset $50. EF: $40, Mbr $30. GMs Free. Reg.: 6:15-6:45pm. Rds.: 7-8:15-9:30-10:45pm.Request at entry. www.marshallchessclub.org. 23 W. 10th St., NYC. 212-477-3716.</p>
               </div>
             </div>
         </div>
@@ -84,6 +77,20 @@
   </div>
 
 
+    <div class="container box">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="item animated bounceInRight">
+            <h1 class="new">News Update</h1>
+            <h5>-Standing Rock Matters. More than 100 gallons of water and 1000 pounds of wood was donated last month.Thank you for all the assistance. We are fighting for water. Thank you so much,please contact us if you would like to donate.</h5>
+
+            <h5>-On Tuesday evening the Bernalillo County Commissioners adopted a public lands resolution and a resolution to cut methane. Our communications director and deputy director spoke in support, read their comments on our website.</h5>
+
+            <h5>-This week, the U.S. Department of the Interior announced a new initiative to work more closely with Native Tribes across the US. Our Deputy Director wrote about the history of these treaties and what this new agreement means and how it can be a big step forward in honoring tribal sovereignty.</h5>
+          </div>
+        </div>
+      </div>
+    </div>
 
     <?php require_once ("lib/partials/scripts.php");?>
 
